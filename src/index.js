7783f0b97fb6166221e0bb9d405df57b3bb808d1
Sung J Byun <sungjbyun@Sungs-MacBook-Pro-2.local>
2017-04-28 22:43:23 -0700

    npm start

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 'use strict';
 
+import React from 'react';
+import { render } from 'react-dom';
+
+import './scss/base.scss';
+import './scss/lib/_theme.scss';
+
+window.React = React;
+
+render (
+    <Contact />,
+    document.getElementById('root')
+)
+
+
